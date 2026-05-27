a9ea9dea5bb52ae7a3a71df0688988c91ea93cf4
Kaushal Sharma <kaushalsharmacs.in@gmail.com>
2026-05-27 06:27:02 +0000

    converrsion of  datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,34 @@
+let score = 33
+let score1 = "33"
+let score2 = "33abc"
+let score3 = null
+
+console.log(typeof score)
+console.log(typeof(score))
+console.log(typeof score1)
+console.log(typeof(score1))
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber)
+
+let valueInNumber1 = Number(score2)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+let valueInNumber2 = Number(score3)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+// true => 1
+
+let isloggedIn = ""
+let booleanIsLoggedIn  = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true; 0=> false
+// "" => false; "kaushal" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
