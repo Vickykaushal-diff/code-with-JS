31d7d29abf49e40e4097a9e8eb21b25ab703cd28
Kaushal Sharma <kaushalsharmacs.in@gmail.com>
2026-05-27 06:54:30 +0000

    operations

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -32,3 +32,20 @@ let booleanIsLoggedIn  = Boolean(isloggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+
+
+// ********** Operations ********
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Kaushal"
+
+let str3 = str1 + " " + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
